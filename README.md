153d121e06e5c2dd233d9255f0d69fad9e89b473
andypotts <andypotts92@gmail.com>
2017-02-03 16:03:48 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # node-chat-app
 Realtime chat app demo using Node.js and Sockets.io.
+
+# Installation
+- Run $ npm install - to install the packages
+- Run $ node app.js - to start the app
+- Visit localhost:8080 in two separate browser windows, and chat between them
---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+
+io.on('connection', function(client) {
+  console.log('client connected');
+
+  client.on('join', function(name){
+    client.nickname = name;
+  });
+
+  client.on('message', function(data){
+    var nickname = client.nickname;
+    client.broadcast.emit("message", nickname + ": " + data);
+    client.emit("message", nickname + ": " + data);
+  });
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(8080);
